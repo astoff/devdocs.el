ebdd3b39bef1a928c069373337d0f8e4f2d8b8d2
Augusto Stoffel <arstoffel@gmail.com>
2024-03-29 08:38:08 +0100

    Version 0.6.1

--- devdocs.el
+++ devdocs.el
@@ -6,7 +6,7 @@
 ;; Keywords: help
 ;; URL: https://github.com/astoff/devdocs.el
 ;; Package-Requires: ((emacs "27.1"))
-;; Version: 0.6
+;; Version: 0.6.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
